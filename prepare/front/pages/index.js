f06ffb99ec8f8f4b39db226acd2f93294febf5da
고윤혁 <sksgur3217@gmail.com>
2022-03-03 10:52:09 +0900

    study naverAPI

--- prepare/front/pages/index.js
+++
@@ -1,7 +0,0 @@
-// GET/ https://openapi.naver.com/v1/search/news.json
-// 요청 변수: query, 타입: string (검색을 원하는 문자열로서 UTF-8로 인코딩한다)
-// http://localhost:3061/
-
-//애플리케이션 정보
-// Client ID: 517uU89Qf9EcWEcI8Ad4
-// Client Secret: 4JTYNsOKDa
